6389912cb107f2d20a985ea714856f7a5a1044db
iurada <iurada@github.com>
2023-03-19 15:59:31 +0100

    Added classification Fish

--- celeba.bat
+++ celeba.bat
@@ -1,5 +1,5 @@
 python main.py^
- --experiment .\experiments\classification\MMD\^
+ --experiment .\experiments\classification\Fish\^
  --dataset .\datasets\classification\CelebA\^
  --data_path .\data\celeba\^
  --max_iters 3000^
